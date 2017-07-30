ddd312e5ab6b45676af78ba14f91cfd773cccbb6
Alexander Korsunsky <fat.lobyte9@gmail.com>
2017-07-30 16:08:14 +0200

    Fix NaN in SimulateAeroProperties for high altitudes

--- FerramAerospaceResearch/FARAeroComponents/FARVesselAero.cs
+++ FerramAerospaceResearch/FARAeroComponents/FARVesselAero.cs
@@ -325,6 +325,8 @@ namespace FerramAerospaceResearch.FARAeroComponents
             {
                 aeroForce = Vector3.zero;
                 aeroTorque = Vector3.zero;
+
+                return;
             }
 
             float velocityMag = velocityWorldVector.magnitude;
